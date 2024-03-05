ac812483e05cd838704b4e4afe184191f7b503ab
abhi t <abhinash.t12@gmail.com>
2024-03-05 08:59:23 -0800

    added linkedin in menu

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,4 +1,4 @@
-import { BookmarkIcon, CameraIcon, Pencil2Icon, PersonIcon, RocketIcon } from '@radix-ui/react-icons'
+import { BookmarkIcon, CameraIcon, GitHubLogoIcon, LinkedInLogoIcon, Pencil2Icon, PersonIcon, RocketIcon } from '@radix-ui/react-icons'
 import {
   Tooltip,
   TooltipContent,
@@ -69,6 +69,30 @@ export function MobileNav() {
           </TooltipTrigger>
           <TooltipContent side={"bottom"} className="bg-black text-white">
             <p>Gallery</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href="https://github.com/nasht12">
+              <div className="flex items-center justify-center m-4 gap-2">
+                <GitHubLogoIcon className="h-6 w-6 md:h-10 md:w-10 border-2 border-black p-1 bg-transparent hover:bg-blue-300 transition-all duration-500 ease-in-out" />
+              </div>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side={"right"} className="bg-black text-white">
+            <p>Github</p>
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href="https://www.linkedin.com/in/abhinashtummala/">
+              <div className="flex items-center justify-center m-4 gap-2">
+                <LinkedInLogoIcon className="h-6 w-6 md:h-10 md:w-10 border-2 border-black p-1 bg-transparent hover:bg-blue-300 transition-all duration-500 ease-in-out" />
+              </div>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side={"right"} className="bg-black text-white">
+            <p>LinkedIn</p>
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
